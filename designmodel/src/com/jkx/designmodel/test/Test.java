71a84f2f677ad8895b0faeccef4effd93f8e1f0d
Administrator <Administrator@PC-20170705VDVK>
2018-05-04 18:25:07 +0800

    up

--- designmodel/src/com/jkx/designmodel/test/Test.java
+++ designmodel/src/com/jkx/designmodel/test/Test.java
@@ -1,7 +1,9 @@
 package com.jkx.designmodel.test;
 
 public class Test {
-   public static void main(String[] args) {
-	 System.out.println("My name is Max,how about you.");
+   public static void main(String[] args){
+	   
+	   System.out.println("My name is Max,how about you.");
+	 
 }
 }
